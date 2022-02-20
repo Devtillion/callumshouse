c65499cd35e39b45e328c8e18811bd93572c944f
Geruls <Gerard.sayson.25@gmail.com>
2022-02-20 20:31:37 +0800

    fix syntax error

--- src/main/java/net/javadiscord/House.java
+++ src/main/java/net/javadiscord/House.java
@@ -16,7 +16,7 @@ public class House {
 
 	public static void main(String[] args) {
 		configureEclipse(); // uncomment if using eclipse
-		var human = new Human("BeamedCallum", true);
+		var human = new Human("BeamedCallum", true, false);
 		var human2 = new Human("CallumsDad", false, true);
 		System.out.println("Does Moon Read Callum Message: " + new Moon().simp(100D, human));
 		System.out.println("Does Pokiman Read Callum Message: " + new Pokiman().simp(100D, human));
