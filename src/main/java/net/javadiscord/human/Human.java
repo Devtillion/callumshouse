fee7c6f4297c4bcb49fdc15b2251da0dcda930d9
Geruls <Gerard.sayson.25@gmail.com>
2022-02-20 20:31:37 +0800

    fix syntax error

--- src/main/java/net/javadiscord/human/Human.java
+++ src/main/java/net/javadiscord/human/Human.java
@@ -4,7 +4,4 @@ package net.javadiscord.human;
  * Creates a human.
  */
 public record Human(String name, boolean abandonedByParents, boolean isGettingMilk) {
-    public Human(String name, boolean abandonedByParents) {
-        super(name, abandonedByParents, false);
-    }
 }
