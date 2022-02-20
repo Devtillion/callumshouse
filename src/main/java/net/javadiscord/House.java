58bf815d693d9f626d9d965980ed15563e47dc8a
Geruls <Gerard.sayson.25@gmail.com>
2022-02-20 20:33:48 +0800

    make callum poor

--- src/main/java/net/javadiscord/House.java
+++ src/main/java/net/javadiscord/House.java
@@ -19,7 +19,7 @@ public class House {
 		var human = new Human("BeamedCallum", true, false);
 		var human2 = new Human("CallumsDad", false, true);
 		System.out.println("Does Moon Read Callum Message: " + new Moon().simp(100D, human));
-		System.out.println("Does Pokiman Read Callum Message: " + new Pokiman().simp(100D, human));
+		System.out.println("Does Pokiman Read Callum Message: " + new Pokiman().simp(50D, human));
 	}
 
 }
